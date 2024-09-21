237a47cf952436efcbe5d7b699bba64286f16fc5
Achyuth2004 <kk9015@srmist.edu.in>
2024-09-21 23:51:13 +0530

    3rd repo

--- 3-variables_input.sh
+++ 3-variables_input.sh
@@ -1,6 +1,8 @@
-PERSON1=ACHYUTH
-PERSON2=AKHILA
+#declaring variable
+PERSON1=$1
+PERSON2=$2
 
+#referring variable
 echo "$PERSON1 : Hi $PERSON2 ,how are you?"
 echo "$PERSON2 : Hi $PERSON1 ,i am fine .How are you?"
 echo "$PERSON1 : I am fine.How is ur work going?"
