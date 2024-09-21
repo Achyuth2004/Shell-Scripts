ff4f723d5ba6eb47b815cdfe128426550a4b27ee
Achyuth2004 <kk9015@srmist.edu.in>
2024-09-21 23:38:12 +0530

    2nd repo

---
+++ 2-variables.sh
@@ -0,0 +1,7 @@
+PERSON1=ACHYUTH
+PERSON2=MANJU
+
+echo "$PERSON1 : Hi $PERSON2 ,how are you?"
+echo "$PERSON2 : Hi $PERSON1 ,i am fine .How are you?"
+echo "$PERSON1 : I am fine.How is ur work going?"
+echo "$PERSON2 : All well!.What abt u"
