d40b2717483943f736aa2a787784eb31edad7bdc
Achyuth2004 <kk9015@srmist.edu.in>
2024-09-22 01:16:03 +0530

    5th repo

---
+++ 5-arithmetic_operators.sh
@@ -0,0 +1,12 @@
+no1=$1
+no2=$2
+
+$sum=$((no1+no2))
+$diff=$((no1-no2))
+$mul=$((no1*no2))
+$div=$((no1/no2))
+
+echo "The sum is $sum"
+echo "The difference is $diff"
+echo "The multiplication is $mul"
+echo "The division is $div"
