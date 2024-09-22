5695685becc91046823499b01004cdd29a6ea16a
Achyuth2004 <kk9015@srmist.edu.in>
2024-09-22 09:50:36 +0530

    special variables repo

---
+++ 6-special_variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "All variables : $@"
+echo "No of varibles are :$#"
+echo "Script Name :$0"
+echo "Current working directory :$PWD"
+echo "Home directory of current user:$HOME"
+echo "User running the script :$USER"
+echo "Hostname:$HOSTNAME"
+echo ""Process ID for current shell script :$$
