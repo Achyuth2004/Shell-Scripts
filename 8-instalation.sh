644ff4fa23537145833bbdc62581f890082ba26b
Achyuth2004 <kk9015@srmist.edu.in>
2024-09-22 13:24:40 +0530

    Installation repo

--- 8-instalation.sh
+++ 8-instalation.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+# Get the user ID of the current user
 userid=$(id -u)
 
+# Check if the user is not root
 if [ $userid -ne 0 ]
 then
     echo "Please proceed with root access."
-    exit 1  #manually exit if error comes
+    exit 1  # Manually exit if not root
 else
-    echo "you are super user"
+    echo "You are super user"
 fi
 
-dnf mysql install -y
+# Correct the dnf command syntax for installing mysql
+dnf install mysql -y
 
+# Check if the previous command (installation) was successful
 if [ $? -ne 0 ]
 then 
-   echo "Installation failure"
+   echo "Installation failed."
    exit 1
 else  
-   echo "Installation completed"
+   echo "Installation completed successfully."
 fi
